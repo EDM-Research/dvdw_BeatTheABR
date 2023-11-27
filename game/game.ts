99bf4fde67db8d30e5378e566d5b0b9e00f3aefd
Mike Vandersanden <mike.vandersanden@uhasselt.be>
2023-11-27 11:29:38 +0100

    turn off debug

--- game/game.ts
+++ game/game.ts
@@ -43,7 +43,7 @@ export class GameElement {
             physics: {
                 default: 'arcade',
                 arcade: {
-                    debug: true,
+                    debug: false,
                 }
             },
             parent: parent,
